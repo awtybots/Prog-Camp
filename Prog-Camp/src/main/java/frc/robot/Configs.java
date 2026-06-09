13b35b09e580cdd56d0413e755369bb6a6f8496a
anax8 <137923544+anax8@users.noreply.github.com>
2026-06-09 10:01:17 -0500

    added hopper
    beeeee

--- Prog-Camp/src/main/java/frc/robot/Configs.java
+++ Prog-Camp/src/main/java/frc/robot/Configs.java
@@ -6,6 +6,8 @@ import com.revrobotics.spark.config.SparkBaseConfig.*;
 
 import frc.robot.Constants.IntakeConstants;
 
+import frc.robot.Constants.HopperConstants;
+
 // import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.FeedbackSensor;
 
@@ -56,6 +58,57 @@ public final class Configs
                             ;
 
                         IntakeMotorRightConfig.closedLoop
+                        .maxMotion.maxAcceleration(1000000);
+
+                }
+
+        };
+
+
+        public static final class HopperSubsystem {
+                
+            public static final SparkFlexConfig HopperMotorLeftConfig = new SparkFlexConfig();
+            public static final SparkFlexConfig HopperMotorRightConfig = new SparkFlexConfig();
+            // public static final SparkFlexConfig IntakeRightMotorConfig = new SparkFlexConfig();
+
+                static {
+
+                        HopperMotorLeftConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+                        HopperMotorRightConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12).follow(HopperConstants.HOPPER_LEFT_ID, true);
+
+
+
+                        HopperMotorLeftConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                            // Set PID values for position control. We don't need to pass a closed
+                            // loop slot, as it will default to slot 0.
+                            .p(HopperConstants.p)
+                            .i(HopperConstants.i)
+                            .d(HopperConstants.d)
+                            .outputRange(-1, 1)
+                            .feedForward
+                            .kS(HopperConstants.s)
+                            .kV(HopperConstants.v)
+                            .kA(HopperConstants.a)
+                            ;
+
+                        HopperMotorLeftConfig.closedLoop
+                        .maxMotion.maxAcceleration(1000000);
+
+
+                        HopperMotorRightConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                            // Set PID values for position control. We don't need to pass a closed
+                            // loop slot, as it will default to slot 0.
+                            .p(HopperConstants.p)
+                            .i(HopperConstants.i)
+                            .d(HopperConstants.d)
+                            .outputRange(-1, 1)
+                            .feedForward
+                            .kS(HopperConstants.s)
+                            .kV(HopperConstants.v)
+                            .kA(HopperConstants.a)
+                            ;
+
+                        HopperMotorRightConfig.closedLoop
                         .maxMotion.maxAcceleration(1000000);
 
                 }
--- Prog-Camp/src/main/java/frc/robot/Constants.java
+++ Prog-Camp/src/main/java/frc/robot/Constants.java
@@ -152,6 +152,29 @@ public final class Constants {
 
   }
 
+// for the Hopper :D
+
+  public static class HopperConstants {
+    public static final int HOPPER_LEFT_ID = 18; // unknown
+    public static final int HOPPER_RIGHT_ID = 19; // unknown
+
+    // PID Constants
+    public static final double p = 0.006155;
+    public static final double i = 0.000;
+    public static final double d = 0.01;
+
+    // Feed-Forward Constants
+    public static final double s = 1.25;
+    public static final double v = 0.5;
+    public static final double a = 0.75;
+
+    public static final double HOPPER_RPM = -12500;
+    public static final double HOPPER_REVERSE_RPM = 12500;
+
+  }
+
+
+
   public static class ShooterConstants {
     public static final int SHOOTER_L1_ID = 9;
     public static final int SHOOTER_L2_ID = 10;
